c5a4e44897bfb1628bf6c8a15cb34744fba7ce0e
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-08-10 23:47:16 +0530

    Update BasicAuthSecurityConfiguration.java

--- learn-spring-security/src/main/java/com/DeepanshuPanwar/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/DeepanshuPanwar/learnspringsecurity/basic/BasicAuthSecurityConfiguration.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -28,5 +32,21 @@ public class BasicAuthSecurityConfiguration {
 
         http.csrf(csrf -> csrf.disable());
         return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService userDetailsService () {
+
+        var user = User.withUsername("DeepanshuPanwar")
+                .password("{noop}password")
+                .roles("USER")
+                .build();
+
+        var admin = User.withUsername("admin")
+                .password("{noop}password")
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
