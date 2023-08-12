df5abe29a94da57a3c97869cfc657ad014f94e2f
Deepanshu Panwar <deepanshu2893@gmail.com>
2023-08-12 17:06:15 +0530

    Update JWTSecurityConfiguration.java

--- learn-spring-security/src/main/java/com/DeepanshuPanwar/learnspringsecurity/jwt/JWTSecurityConfiguration.java
+++ learn-spring-security/src/main/java/com/DeepanshuPanwar/learnspringsecurity/jwt/JWTSecurityConfiguration.java
@@ -1,22 +1,35 @@
 package com.DeepanshuPanwar.learnspringsecurity.jwt;
 
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.KeySourceException;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.List;
+import java.util.UUID;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -83,8 +96,35 @@ public class JWTSecurityConfiguration {
     }
 
     @Bean
-    public JwtDecoder jwtDecoder () {
+    public KeyPair keyPair() {
+        try {
+            var keyPairGenerator = KeyPairGenerator.getInstance(("RSA"));
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (Exception ex){
+            throw new RuntimeException(ex);
+        }
+    }
 
+    @Bean
+    public RSAKey rsaKey () {
+        return new RSAKey
+                .Builder((RSAPublicKey) keyPair().getPublic())
+                .privateKey(keyPair().getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    @Bean
+    public JWKSource <SecurityContext> jwkSource(RSAKey rsaKey) {
+        var jwkSet = new JWKSet(rsaKey);
+
+        return (jwkSelector, context) -> jwkSelector.select(jwkSet);
+    }
+    @Bean
+    public JwtDecoder jwtDecoder (RSAKey rsaKey) throws JOSEException {
+return NimbusJwtDecoder.withPublicKey(rsaKey().toRSAPublicKey())
+        .build();
     }
 
 }
